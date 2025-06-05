780f71229a5154e624ef3820c1d04173fced61d1
Izaak <izaakhaliq@gmail.com>
2025-06-05 13:37:20 +0800

    Update layout

---
+++ pages/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} title="Data Quality" homePageName="KK - FMM" logo="lzs-logo.png" builtWithEvidence=false hideBreadcrumbs=true>
+	<slot slot="content" />
+</EvidenceDefaultLayout>
